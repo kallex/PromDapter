ed8b6e10335d10afb0ba2b5f2fca6913b15018ed
Kalle Launiala <kalle.launiala@protonit.net>
2020-04-01 23:53:49 +0300

    Updated tests, and latest DataProvider

---
+++ src/PromDapterTests/Controllers/MetricsControllerTests.cs
@@ -0,0 +1,72 @@
+﻿using Xunit;
+using PromDapterSvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.dotMemoryUnit;
+using PrometheusProcessor;
+using SensorMonHTTP;
+using Xunit.Abstractions;
+
+namespace PromDapterSvc.Controllers.Tests
+{
+    public class MetricsControllerTests
+    {
+        public MetricsControllerTests(ITestOutputHelper outputHelper)
+        {
+            DotMemoryUnitTestOutput.SetOutputMethod(
+                message => outputHelper.WriteLine(message));
+        }
+
+
+        [Fact]
+        public async Task GetMemoryLeakTest()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var controller = new MetricsController(null, null);
+                var result = await controller.Get();
+            }
+            assertMemoryState();
+        }
+
+        [Fact]
+        public async Task GetPerformanceProfileTest()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                var controller = new MetricsController(null, null);
+                var result = await controller.Get();
+            }
+        }
+
+
+
+        private void assertMemoryState()
+        {
+            expectAmount(0,
+                typeof(MemoryMappedViewAccessor),
+                typeof(GCHandle),
+                typeof(HWiNFOProvider));
+
+            expectAmount(1, typeof(YamlRegexMapData));
+
+            void expectAmount(int expectedCount, params Type[] types)
+            {
+                foreach (var type in types)
+                {
+                    dotMemory.Check(memory =>
+                    {
+                        var actualCount = memory.GetObjects(item => item.Type.Is(type)).ObjectsCount;
+                        Assert.True(expectedCount == actualCount,
+                            $"{type.Name}: expected {expectedCount} - had {actualCount}");
+                    });
+                }
+            }
+        }
+
+    }
+}
--- src/PromDapterTests/ServiceProcessorTests.cs
+++ src/PromDapterTests/ServiceProcessorTests.cs
@@ -1,26 +1,80 @@
 using System;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using JetBrains.dotMemoryUnit;
 using PrometheusProcessor;
 using SensorMonHTTP;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace PromDapterTests
 {
     public class ServiceProcessorTests
     {
+        public ServiceProcessorTests(ITestOutputHelper outputHelper)
+        {
+            DotMemoryUnitTestOutput.SetOutputMethod(
+                message => outputHelper.WriteLine(message));
+        }
+
+
         [Fact]
         public async Task DataItemRegexProcessorTest()
         {
-            var serviceProcessor = new ServiceProcessor();
-            serviceProcessor.InitializeProcessors();
-            var processor = serviceProcessor.DataItemRegexProcessor;
-            var service = new HWiNFOProvider();
-            string[] result;
-            //for (int i = 0; i < 10000; i++)
+            string[] result = null;
+            for (int i = 0; i < 1000; i++)
             {
+                var serviceProcessor = new ServiceProcessor();
+                serviceProcessor.InitializeProcessors();
+                var processor = serviceProcessor.DataItemRegexProcessor;
+                var service = new HWiNFOProvider();
                 result = await processor(service);
             }
             var metricDump = String.Join(Environment.NewLine, result);
         }
+
+
+        [Fact]
+        public async Task ServiceProcessorMemoryLeakTest()
+        {
+            string[] result = null;
+            for (int i = 0; i < 100; i++)
+            {
+                var serviceProcessor = new ServiceProcessor();
+                serviceProcessor.InitializeProcessors();
+                var processor = serviceProcessor.DataItemRegexProcessor;
+                var service = new HWiNFOProvider();
+                result = await processor(service);
+            }
+            var metricDump = String.Join(Environment.NewLine, result);
+            assertMemoryState();
+        }
+
+
+        private void assertMemoryState()
+        {
+            expectAmount(0, 
+                typeof(MemoryMappedViewAccessor),
+                typeof(GCHandle));
+            
+            expectAmount(1, typeof(HWiNFOProvider), typeof(YamlRegexMapData));
+            
+            void expectAmount(int expectedCount, params Type[] types)
+            {
+                foreach (var type in types)
+                {
+                    dotMemory.Check(memory =>
+                    {
+                        var actualCount = memory.GetObjects(item => item.Type.Is(type)).ObjectsCount;
+                        Assert.True(expectedCount == actualCount ,
+                            $"{type.Name}: expected {expectedCount} - had {actualCount}");
+                    });
+                }
+            }
+        }
+
+
     }
 }
