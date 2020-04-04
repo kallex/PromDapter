bd8d75c8079f1b3e3cbc39c7c23178c695acfd4c
Kalle Launiala <kalle.launiala@protonit.net>
2020-04-04 21:53:44 +0300

    Tuned installation settings

---
+++ src/PromDapterSvc/ObfuscationSettings.cs
@@ -0,0 +1,4 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: Obfuscation(Feature = "merge with PrometheusProcessor.dll", Exclude = false)]
---
+++ src/PrometheusProcessor/ObfuscationSettings.cs
@@ -0,0 +1,4 @@
+﻿using System;
+using System.Reflection;
+
+
