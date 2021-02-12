64f9fd4f528d41a9ce6c6996729b910760edc19b
Kalle Launiala <kalle.launiala@protonit.net>
2021-02-12 20:52:33 +0200

    Added host name for metric metadata, attempt to fix the duplicate # HELP tags

--- src/PrometheusProcessor/ServiceProcessor.cs
+++ src/PrometheusProcessor/ServiceProcessor.cs
@@ -19,6 +19,8 @@ namespace PrometheusProcessor
     public class ServiceProcessor
     {
         public delegate Task<(string mimeType, object data)> Processor(IPromDapterService service, object parameters);
+
+        private string CurrentHostName = Environment.MachineName;
         public ServiceProcessor()
         {
 
@@ -68,7 +70,9 @@ namespace PrometheusProcessor
                 // Above 4 we're starting to get diminishing returns; tested on 16 Core Ryzen 3950X with 370 metrics
                 // thus we estimate around 100 per core is sensible metric worthwhile the scheduling
                 var degreeOfParallelism = (int) Math.Ceiling(itemCount / MinimumAmountPerCore);
-                var parallelBag = new ConcurrentBag<(string helpString, string typeString, string[] metricLines)>();
+                var parallelBag = new ConcurrentBag<(string helpKey, string typeString, string[] metricLines)>();
+                // Dicting helpDict to avoid errors on multiple helps
+                var helpDict = new ConcurrentDictionary<string, string>();
 
                 Parallel.ForEach(dataItems, new ParallelOptions()
                     {
@@ -94,6 +98,7 @@ namespace PrometheusProcessor
                         metadataDict.Add("sensor_type", sensorType);
                         metadataDict.Add("sensor", sensorName);
                         metadataDict.Add("source", sourceName);
+                        metadataDict.Add("host", CurrentHostName);
 
                         // Move Entity name as prefix for MetricName
                         if (metadataDict.ContainsKey("Entity"))
@@ -108,7 +113,11 @@ namespace PrometheusProcessor
                         }
 
                         var finalMetricName = $"{providerMetrixPrefix}{cleanupMetricName(metricName)}";
-                        var helpString = $"# HELP {finalMetricName} {metricName.Replace("_", " ")} - {sourceName}";
+                        var helpKey = $"# HELP {finalMetricName}";
+                        var helpString = $"{helpKey} {metricName.Replace("_", " ")} - {sourceName}";
+                        var multipleSourceHelpString = $"{helpKey} {metricName.Replace("_", " ")} - (multiple sources)";
+                        // If already exists with different name, add multiple sources name
+                        helpDict.AddOrUpdate(helpKey, helpString, (key, oldValue) => helpString == oldValue ? oldValue : multipleSourceHelpString);
                         var metricType = getMetrictType(unit);
                         string typeString = null;
                         if (metricType != null)
@@ -122,13 +131,13 @@ namespace PrometheusProcessor
                         var categoryString = String.Join(",", categoryParts);
                         var promStr =
                             $"{finalMetricName}{{{categoryString}}} {Convert.ToString(item.Value.Object, CultureInfo.InvariantCulture)}";
-                        parallelBag.Add((helpString, typeString, new[] {promStr}));
+                        parallelBag.Add((helpKey, typeString, new[] {promStr}));
                     });
                 var grouped = parallelBag
-                    .OrderBy(item => item.helpString).ThenBy(item => item.typeString)
-                    .GroupBy(item => (item.helpString, item.typeString));
+                    .OrderBy(item => helpDict[item.helpKey]).ThenBy(item => item.typeString)
+                    .GroupBy(item => (helpDict[item.helpKey], item.typeString));
                 var lineResult = grouped
-                    .SelectMany(grp => new[] {grp.Key.helpString, grp.Key.typeString}
+                    .SelectMany(grp => new[] {grp.Key.Item1, grp.Key.typeString}
                         .Concat(grp.SelectMany(item => item.metricLines).OrderBy(item => item))
                         .Where(item => !String.IsNullOrEmpty(item))).ToArray();
                 var result = String.Join(Environment.NewLine, lineResult);
