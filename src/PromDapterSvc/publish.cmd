62c9dc8bf43fddd74638bffdf0b4924a0b468389
Kalle Launiala <kalle.launiala@protonit.net>
2021-02-14 22:10:22 +0200

    Fixed publish.cmd to publish to project root (to be in sync with DevOps building)

--- src/PromDapterSvc/publish.cmd
+++ src/PromDapterSvc/publish.cmd
@@ -1,3 +1,3 @@
-rmdir /s /q bin\Release\netcoreapp3.1\win10-x64\publish
-dotnet publish PromDapterSvc.csproj -c Release --self-contained --runtime win10-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
-copy Prometheusmapping.yaml bin\Release\netcoreapp3.1\win10-x64\publish\
+rmdir /s /q ..\..\src\..\publish
+dotnet publish PromDapterSvc.csproj -c Release --self-contained --runtime win10-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true -o ..\..\src\..\publish
+copy Prometheusmapping.yaml ..\..\src\..\publish
