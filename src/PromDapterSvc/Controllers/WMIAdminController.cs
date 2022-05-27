68c7e1512f9905088fcfe3b4a432a25f1dc0afb7
Kalle Launiala <kalle.launiala@protonit.net>
2022-05-28 00:33:27 +0300

    WIP: WMIAdmin RazorPages initially working

---
+++ src/PromDapterSvc/Controllers/WMIAdminController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PromDapterSvc.Controllers
+{
+    public class WMIAdminController : Controller
+    {
+        // GET: WMIAdminController
+        [HttpGet]
+        [Route("WMIAdmin")]
+        //[Route("{filter}")]
+        public ActionResult Index(string filter = null)
+        {
+            return View();
+        }
+
+        // GET: WMIAdminController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: WMIAdminController/Create
+        [HttpGet]
+        [Route("WMIAdmin/Create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: WMIAdminController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WMIAdminController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: WMIAdminController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WMIAdminController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: WMIAdminController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- src/PromDapterSvc/Startup.cs
+++ src/PromDapterSvc/Startup.cs
@@ -24,7 +24,7 @@ namespace PromDapterSvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(options => options.EnableEndpointRouting = false);
             services.AddCors(options =>
             {
                 options.AddDefaultPolicy(builder =>
@@ -34,6 +34,7 @@ namespace PromDapterSvc
                     builder.AllowAnyOrigin();
                 });
             });
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,11 +49,14 @@ namespace PromDapterSvc
 
             app.UseCors();
 
+            //app.UseMvc(builder => builder.ApplicationBuilder.);
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapRazorPages();
             });
         }
     }
