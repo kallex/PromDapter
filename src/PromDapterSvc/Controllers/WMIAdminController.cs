b08e249cbcc3164593662b59b545751959373030
Kalle Launiala <kalle.launiala@protonit.net>
2022-05-28 00:47:38 +0300

    WIP: WMI namespace and class listings in WMIAdminController

--- src/PromDapterSvc/Controllers/WMIAdminController.cs
+++ src/PromDapterSvc/Controllers/WMIAdminController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PromDapterSvc.Controllers
@@ -11,6 +16,20 @@ namespace PromDapterSvc.Controllers
         //[Route("{filter}")]
         public ActionResult Index(string filter = null)
         {
+            var namespaces = GetWmiNamespaces("root");
+
+            List<(string namespaceName, string className)> fullClassNames =
+                new List<(string namespaceName, string className)>();
+            foreach (String namespaceName in namespaces)
+            {
+                var classNames = GetClassNamesWithinWmiNamespace(namespaceName);
+                foreach (var className in classNames)
+                {
+                    Debug.WriteLine($"{namespaceName}:{className}");
+                    fullClassNames.Add((namespaceName, className));
+                }
+
+            }
             return View();
         }
 
@@ -84,5 +103,44 @@ namespace PromDapterSvc.Controllers
                 return View();
             }
         }
+
+        private static List<String> GetWmiNamespaces(string root)
+        {
+            List<String> namespaces = new List<string>();
+            try
+            {
+                ManagementClass nsClass = new ManagementClass(new ManagementScope(root), new ManagementPath("__namespace"), null);
+                foreach (ManagementObject ns in nsClass.GetInstances())
+                {
+                    string namespaceName = root + "\\" + ns["Name"].ToString();
+                    namespaces.Add(namespaceName);
+                    namespaces.AddRange(GetWmiNamespaces(namespaceName));
+                }
+            }
+            catch (ManagementException me)
+            {
+                Debug.WriteLine(me.Message);
+            }
+
+            return namespaces.OrderBy(s => s).ToList();
+        }
+
+        private static List<String> GetClassNamesWithinWmiNamespace(string wmiNamespaceName)
+        {
+            List<String> classes = new List<string>();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher
+            (new ManagementScope(wmiNamespaceName),
+                new WqlObjectQuery("SELECT * FROM meta_class"));
+            List<string> classNames = new List<string>();
+            ManagementObjectCollection objectCollection = searcher.Get();
+            foreach (ManagementClass wmiClass in objectCollection)
+            {
+                string stringified = wmiClass.ToString();
+                string[] parts = stringified.Split(new char[] { ':' });
+                classes.Add(parts[1]);
+            }
+            return classes.OrderBy(s => s).ToList();
+        }
     }
 }
+
